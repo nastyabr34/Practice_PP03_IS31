59a109e44104690c92f24ef40152785bcd6a6308
nastyabr34 <anastasiaaabr34@gmail.com>
2024-12-11 12:29:51 +0300

    Update MainModule.py

--- Src/MainModule.py
+++ Src/MainModule.py
@@ -1,16 +1,7 @@
-#Модуль запуска приложения
+import tkinter as tk
+from app import ImageConverterApp
 
-from SampleModule import *
-
-#Отображение данных
-def show(*results):
-    for result in results:
-        print(result)
-
-#Запуск приложения
-if __name__ == "__main__":
-
-    result_1 = sum_numbers(1, 2, 3, 4, 5)
-    result_2 = sum_numbers(55, 66, 77, 88, 99)
-    
-    show(result_1, result_2)
+if name == "__main__":
+    root = tk.Tk()
+    app = ImageConverterApp(root)
+    root.mainloop()
